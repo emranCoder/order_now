0ce4a45e1ed5fe2cdabd774d3224b59b33fdf8cf
emrancoder <shaheyalam74@gmail.com>
2024-05-20 20:15:44 +0600

    news add for frontend view

--- frontend/src/component/News.jsx
+++ frontend/src/component/News.jsx
@@ -1,42 +1,71 @@
 import React, { useEffect, useState } from "react";
 import Loading from "./Loading";
+import axios from "axios";
 
 export default function News() {
   const [loader, setLoader] = useState(true);
+  const [news, setNews] = useState(null);
+  const [search, setSearch] = useState("");
   useEffect(() => {
     window.scrollTo(0, 0);
     setTimeout(() => {
       setLoader(false);
+      getNews();
     }, 500);
   }, [0]);
 
-  const items = () => (
-    <div className="card rounded-none card-side bg-base-100 border-b pb-2 mb-8">
+  const getNews = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/api/news/all`);
+      if (response && response.status === 200) {
+        console.log();
+        setNews(response.data.news);
+      }
+    } catch (error) {
+      if (error.message === "Network Error")
+        return console.error(error.message);
+      console.log(error.response.data.message);
+    }
+  };
+
+  const items = (val, key) => (
+    <div
+      key={key}
+      className="card rounded-none card-side bg-base-100 border-b pb-2 mb-8"
+    >
       <div className="avatar">
         <div className="w-56">
-          <img src="https://pinchofyum.com/cdn-cgi/image/width=360,height=360,fit=crop/wp-content/uploads/Crispy-Air-Fryer-Tofu.jpg" />
+          <img
+            src={`http://localhost:5000/news/img/${
+              val.image ? val.image : "default-product.png"
+            }`}
+          />
         </div>
       </div>
       <div className="card-body py-0 gap-y-5 ">
         <span className="text-slate-500 font-serif font-semibold">
-          {" "}
-          February 26, 2024
+          {new Date(val.createdAt).toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+          })}
+          ,{" "}
+          {new Date(val.createdAt).toLocaleTimeString("en-us", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
         </span>
         <h2 className="card-title text-slate-800 text-3xl font-serif">
           <a href="#" className="hover:underline">
-            {" "}
-            Ridiculously Good Air Fryer Tofu
+            {val.title}
           </a>
         </h2>
-        <p className="text-slate-600">
-          The crispiest air fryer tofu – in 15 minutes! Extremely easy,
-          incredibly versatile, and an absolute weeknight go-to.
-        </p>
-        <div className="card-actions flex-auto">
+        <p className="text-slate-600">{val.description}</p>
+        {/* <div className="card-actions flex-auto">
           <button className="btn mt-0 h-auto min-h-[auto] text-lg !border-none hover:text-slate-800 !bg-transparent text-red-800 uppercase tracking-wide m-0 p-0 font-sans">
             Continue Reading
           </button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
@@ -49,33 +78,82 @@ export default function News() {
             <h5 className="mb-8 text-red-800 font-semibold uppercase font-serif">
               The Latest & Greatest
             </h5>
-            {items()}
-            {items()}
-            {items()}
-            {items()}
+            {news &&
+              news
+                .filter((val) => {
+                  return search
+                    ? new Date(val.createdAt).getDate() === search
+                    : val;
+                })
+                .map((val, key) =>
+                  val ? (
+                    items(val, key)
+                  ) : (
+                    <p className="grid justify-center content-center h-full text-slate-600 text-lg">
+                      No Data Found
+                    </p>
+                  )
+                )}
+            {!news && (
+              <p className="grid justify-center content-center h-full text-slate-600 text-lg">
+                No Data Found
+              </p>
+            )}
           </div>
           <div className="col-lg-4">
             <h5 className="mb-8 text-red-800 font-semibold uppercase font-serif">
               News Collections
             </h5>
             <ul className="">
-              <li className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700">
+              <li
+                onClick={() => {
+                  if (news) {
+                    setSearch("");
+                  }
+                }}
+                className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700"
+              >
                 <span className="mr-5 rounded-full !bg-red-800 text-white btn-circle btn btn-xs border-none">
                   1
                 </span>
-                <a>Most Viewed</a> <span className="float-right mr-5">48</span>
+                <a>Most Viewed</a>{" "}
+                <span className="float-right mr-5"> {news && news.length}</span>
               </li>
-              <li className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700">
+              <li
+                onClick={() => {
+                  if (news) {
+                    setSearch("");
+                  }
+                }}
+                className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700"
+              >
                 <span className="mr-5 rounded-full !bg-red-800 text-white btn-circle btn btn-xs border-none">
                   2
                 </span>
-                <a>Latest</a> <span className="float-right mr-5">48</span>
+                <a>Previous</a>{" "}
+                <span className="float-right mr-5">{news && news.length}</span>
               </li>
-              <li className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700">
+              <li
+                onClick={() => {
+                  if (news) {
+                    setSearch(new Date().getDate());
+                  }
+                }}
+                className="bg-base-200 mb-1 p-2 font-serif hover:text-red-800 hover:bg-base-300  px-5 cursor-pointer text-slate-700"
+              >
                 <span className="mr-5 rounded-full !bg-red-800 text-white btn-circle btn btn-xs border-none">
                   3
                 </span>
-                <a>Today</a> <span className="float-right mr-5">48</span>
+                <a>Today</a>{" "}
+                <span className="float-right mr-5">
+                  {news &&
+                    news.filter((val) => {
+                      return (
+                        new Date(val.createdAt).getDate() ===
+                        new Date().getDate()
+                      );
+                    }).length}
+                </span>
               </li>
             </ul>
           </div>
